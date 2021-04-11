40d2a06710f8b4da236583881590cde0afe04391
Sandra <sandrav0609@gmail.com>
2021-04-10 22:52:04 -0400

    implement heapsort

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from search.utils import *
 from sort.utils import *
 
 def main():
-    A = [3,1,7,12,67,45,8,2,10]
-    print("A:", A)
+    A = [3,1,7,12,67,45,8,2,10,6]
+    print("A: ", A)
     # insertion_sort(A)
     # print("Sorting A using insertion sort: ", A)
     # x = 38
     # print("Index of ", x, "in A ", A, " using binary search: ", binary_search(A, x))
-    mergesort(A, 0, len(A)-1)
-    print("Sorting A using mergesort: ", A)
+    # mergesort(A, 0, len(A)-1)
+    # print("Sorting A using mergesort: ", A)
+    # heap = Heap(A)
+    # print("Heap from A: ", heap.heap)
+    # print("Max extraction: ", heap.extract_max())
+    # print("Heap now: ", heap.heap)
+    print("Sorting A using heapsort: ", heapsort(A))
 
 main()
--- sort/utils.py
+++ sort/utils.py
@@ -55,3 +55,72 @@ def mergesort(arr, start, end):
         mergesort(arr, start, middle)
         mergesort(arr, middle+1, end)
         merge(arr, start, middle, end)
+
+class Heap:
+    """ Max heap data structure implementation """
+
+    def __init__(self, arr=None):
+        """ Constructor """
+        self.heap = arr
+        self.size = len(arr)
+        for i in range(self.size//2, -1, -1):
+            self.max_heapify(i)
+
+    def max_heapify(self, x):
+        """ Restores heap property at position x
+        
+            O(log(n))
+        """
+        left = 2*x # Left tree
+        right = 2*x + 1 # Right tree
+        largest_index = x
+        if left < self.size and self.heap[left] > self.heap[x]:
+            largest_index = left
+        if right < self.size and self.heap[right] > self.heap[largest_index]:
+            largest_index = right
+        if largest_index != x:
+            self.heap[largest_index], self.heap[x] = self.heap[x], self.heap[largest_index]
+            self.max_heapify(largest_index)
+
+    def maximum(self):
+        """ Returns max element """
+        if self.heap:
+            return self.heap[0]
+        return None
+
+    def extract_max(self):
+        """ Extracts maximum element from heap """
+        maximum = self.heap[0]
+        self.heap[0] = self.heap[self.size-1]
+        del self.heap[self.size-1]
+        self.size -= 1
+        self.max_heapify(0)
+        return maximum
+
+    def increase_key(self, x, key):
+        """ Increases key of element at position x to value key
+
+            Edge case with 0 and 1
+        """
+        self.heap[x] = key
+        while x > 0 and self.heap[x//2] < self.heap[x]: # Check parent
+            self.heap[x//2], self.heap[x] = self.heap[x], self.heap[x//2]
+            x = x//2
+
+    def insert(self, key):
+        """ Inserts element with priority key inside heap """
+        self.size += 1
+        self.heap.append(-math.inf)
+        self.increase_key(self.size-1, key)
+
+def heapsort(arr):
+    """ Implements heapsort
+    
+        O(n*log(n))
+    """
+    heap = Heap(arr)
+    for i in range(heap.size-1, 0, -1):
+        heap.heap[0], heap.heap[heap.size-1] = heap.heap[heap.size-1], heap.heap[0]
+        heap.size -= 1
+        heap.max_heapify(0)
+    return heap.heap # Ordered array
